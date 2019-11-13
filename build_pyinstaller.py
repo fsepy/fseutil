53d13170996f76dd53f1a0c196ab49c3e1e85af6
fuyans <fuyans@gmail.com>
2019-11-13 23:33:18 +0000

    build script.

---
+++ build_pyinstaller.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import os
+import subprocess
+
+
+def build_gui():
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+    cmd_pyinstaller = 'python -m PyInstaller'
+    cmd_option_list = [
+        '--noconsole',
+        '--onefile',
+        '--windowed',
+    ]
+    cmd_extra_files_list = [
+
+    ]
+    cmd_script = 'gui.py'
+
+    cmd_options = ' '.join(cmd_option_list)
+    cmd_extra_files = ' '.join(cmd_extra_files_list)
+
+    cmd_options = cmd_options + ' ' if len(cmd_options) > 0 else ''
+    cmd_extra_files = cmd_extra_files + ' ' if len(cmd_extra_files) > 0 else ''
+
+    cmd = f'{cmd_pyinstaller} {cmd_options}{cmd_extra_files}{cmd_script}'
+
+    print(cmd)
+    subprocess.call(cmd)
+
+
+if __name__ == '__main__':
+    build_gui()
