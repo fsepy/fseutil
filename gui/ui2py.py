acc33bfaab435cddf240e287cce23d7d85a9adcf
fuyans <fuyans@gmail.com>
2020-01-22 23:19:46 +0000

    clean old gui files.

---
+++ gui/ui2py.py
@@ -0,0 +1,22 @@
+
+
+def ui2py():
+    import os
+    import subprocess
+
+    list_ui_file_names = [
+        'main.ui',
+        'dialog_4_1_br187_parallel.ui'
+    ]
+
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    destination_dir = os.path.join(os.path.dirname(cwd), 'fseutil', 'ui')
+
+    for ui_file_name in list_ui_file_names:
+        cmd = f'pyside2-uic {os.path.join(cwd, ui_file_name)} > {os.path.join(destination_dir, ui_file_name.replace(".ui", ".py"))}'
+        print(cmd)
+        subprocess.call(cmd, shell=True)
+
+
+if __name__ == '__main__':
+    ui2py()
