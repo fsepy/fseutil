132acf950d0bcfe91b077bb8c4773adf30d28b25
fuyans <fuyans@gmail.com>
2020-02-04 13:44:09 +0000

    general improvements.

--- pyinstaller_builds/gui_build.py
+++ pyinstaller_builds/gui_build.py
@@ -3,24 +3,22 @@ import os
 import subprocess
 
 
-def build_gui():
+def build_gui(app_name: str = 'OFRUTIL', fp_target_py: str = 'gui.py'):
 
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
     cmd_option_list = [
-        '-n "OFR B4 BR187 Calculator"',
+        f'-n "{app_name}"',
         "--noconsole",
         "--onefile",
         "--windowed",
         "--icon=" + os.path.realpath(os.path.join("etc", "ofr-colour-618_618.ico")),
     ]
-    cmd_script = "gui.py"
 
-    cmd_pyinstaller = "pyinstaller"
     cmd_options = " ".join(cmd_option_list)
     cmd_options = cmd_options + " " if len(cmd_options) > 0 else ""
 
-    cmd = f"{cmd_pyinstaller} {cmd_options}{cmd_script}"
+    cmd = f"pyinstaller {cmd_options}{fp_target_py}"
 
     subprocess.call(cmd, shell=True)
 
