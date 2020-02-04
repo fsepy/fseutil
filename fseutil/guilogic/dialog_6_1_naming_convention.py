ee6f88f6f2e287a18d5769402931e8e0914bd30c
fuyans <fuyans@gmail.com>
2020-02-04 21:32:38 +0000

    Update dialog_6_1_naming_convention.py

--- fseutil/guilogic/dialog_6_1_naming_convention.py
+++ fseutil/guilogic/dialog_6_1_naming_convention.py
@@ -1,4 +1,4 @@
-from PySide2 import QtWidgets
+from PySide2 import QtWidgets, QtCore, QtGui
 from fseutil.guilayout.dialog_6_1_naming_convention import Ui_dialog_6_1_naming_convention
 
 
@@ -8,6 +8,32 @@ class Dialog0601_NamingConvention(QtWidgets.QDialog):
         self.ui = Ui_dialog_6_1_naming_convention()
         self.ui.setupUi(self)
 
-    def update_file_full_name(self):
-        # self.ui.label_out
-        pass
+        self.make_file_name()
+
+        self.ui.lineEdit_1_date.textChanged.connect(self.make_file_name)
+        self.ui.comboBox_2_revision.currentTextChanged.connect(self.make_file_name)
+        self.ui.lineEdit_3_project_no.textChanged.connect(self.make_file_name)
+        self.ui.lineEdit_4_project_stage.textChanged.connect(self.make_file_name)
+        self.ui.lineEdit_5_title.textChanged.connect(self.make_file_name)
+        self.ui.comboBox_6_type.currentTextChanged.connect(self.make_file_name)
+        self.ui.comboBox_7_security_status.currentTextChanged.connect(self.make_file_name)
+
+        self.ui.pushButton_copy.clicked.connect(self.copy_file_name)
+
+        self.repaint()
+
+    def make_file_name(self):
+        aa = self.ui.lineEdit_1_date.text()
+        bb = self.ui.comboBox_2_revision.currentText()[0:3]
+        cc = self.ui.lineEdit_3_project_no.text()
+        dd = self.ui.lineEdit_4_project_stage.text()
+        ee = self.ui.lineEdit_5_title.text()
+        ff = self.ui.comboBox_6_type.currentText()[0:2]
+        gg = self.ui.comboBox_7_security_status.currentText()[0:3]
+
+        self.ui.lineEdit_result.setText('-'.join([aa, bb, cc, dd, ee, ff, gg]))
+
+    def copy_file_name(self):
+        clipboard = QtGui.QGuiApplication.clipboard()
+        clipboard.setText(self.ui.lineEdit_result.text())
+        self.ui.lineEdit_result.selectAll()
