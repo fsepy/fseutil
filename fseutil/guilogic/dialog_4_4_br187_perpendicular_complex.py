52273b9dc4e9498c2a0ad46c1d360872b279e113
fuyans <fuyans@gmail.com>
2020-02-05 00:17:32 +0000

    added ui logics for 0403 and 0404.

--- fseutil/guilogic/dialog_4_4_br187_perpendicular_complex.py
+++ fseutil/guilogic/dialog_4_4_br187_perpendicular_complex.py
@@ -1,21 +1,21 @@
 from PySide2 import QtWidgets, QtGui, QtCore
 
-from fseutil.etc.images_base64 import dialog_4_3_br187_parallel_figure_1
+from fseutil.etc.images_base64 import dialog_4_4_br187_perpendicular_figure_1
 from fseutil.guilayout.dialog_4_1_br187_parallel_simple import Ui_dialog_4_1_br187_parallel_simple
-from fseutil.lib.fse_thermal_radiation import phi_parallel_any_br187, linear_solver
+from fseutil.lib.fse_thermal_radiation import phi_perpendicular_any_br187, linear_solver
 
 
-class Dialog0403_br187ParallelComplex(QtWidgets.QDialog):
+class Dialog0404_br187PerpendicularComplex(QtWidgets.QDialog):
 
     maximum_acceptable_thermal_radiation_heat_flux = 12.6
 
     def __init__(self, parent=None):
-        super(Dialog0403_br187ParallelComplex, self).__init__(parent)
+        super(Dialog0404_br187PerpendicularComplex, self).__init__(parent)
         self.ui = Ui_dialog_4_1_br187_parallel_simple()
         self.ui.setupUi(self)
 
         # set up radiation figure
-        ba = QtCore.QByteArray.fromBase64(dialog_4_3_br187_parallel_figure_1)
+        ba = QtCore.QByteArray.fromBase64(dialog_4_4_br187_perpendicular_figure_1)
         pix_map = QtGui.QPixmap()
         pix_map.loadFromData(ba)
         self.ui.label.setPixmap(pix_map)
@@ -23,6 +23,19 @@ class Dialog0403_br187ParallelComplex(QtWidgets.QDialog):
         self.ui.comboBox_S_or_UA.currentTextChanged.connect(self.change_mode_S_and_UA)
         self.ui.pushButton_calculate.clicked.connect(self.calculate)
         self.ui.pushButton_test.clicked.connect(self.test)
+
+    def test(self):
+
+        self.ui.lineEdit_W.setText('50')
+        self.ui.lineEdit_H.setText('50')
+        self.ui.lineEdit_Q.setText('84')
+        self.ui.comboBox_S_or_UA.setCurrentIndex(0)
+        self.change_mode_S_and_UA()
+        self.ui.lineEdit_S_or_UA.setText('2')
+
+        self.calculate()
+
+        self.repaint()
 
     def change_mode_S_and_UA(self):
         """update ui to align with whether to calculate boundary distance or unprotected area %"""
@@ -49,19 +62,6 @@ class Dialog0403_br187ParallelComplex(QtWidgets.QDialog):
 
         self.repaint()
 
-    def test(self):
-
-        self.ui.lineEdit_W.setText('10')
-        self.ui.lineEdit_H.setText('10')
-        self.ui.lineEdit_Q.setText('84')
-        self.ui.comboBox_S_or_UA.setCurrentIndex(0)
-        self.change_mode_S_and_UA()
-        self.ui.lineEdit_S_or_UA.setText('2')
-
-        self.calculate()
-
-        self.repaint()
-
     def calculate(self):
 
         # clear ui output fields
@@ -85,7 +85,7 @@ class Dialog0403_br187ParallelComplex(QtWidgets.QDialog):
             S = max(1, min([S, 200]))
             self.ui.lineEdit_S_or_UA.setText(f'{S/2:.2f}')
 
-            phi_solved = phi_parallel_any_br187(W_m=W, H_m=H, w_m=0.5*W, h_m=0.5*H, S_m=S)
+            phi_solved = phi_perpendicular_any_br187(W_m=W, H_m=H, w_m=0., h_m=0., S_m=S)
             q_solved = Q * phi_solved
             UA_solved = max([min([q_target/q_solved * 100, 100]), 0])
 
@@ -93,28 +93,29 @@ class Dialog0403_br187ParallelComplex(QtWidgets.QDialog):
             self.ui.lineEdit_out_q.setText(f'{q_solved:.2f}')
             self.ui.lineEdit_out_S_or_UA.setText(f'{UA_solved:.2f}')
 
-        elif self.ui.comboBox_S_or_UA.currentText() == 'UA':  # to calculate minimum separation distance to boundary
+        # to calculate minimum separation distance to boundary
+        elif self.ui.comboBox_S_or_UA.currentText() == 'UA':
             UA = float(self.ui.lineEdit_S_or_UA.text()) / 100.
             UA = max([0.0001, min([UA, 1])])
-            self.ui.lineEdit_S_or_UA.setText(f'{UA*100:.0f}')
+            self.ui.lineEdit_S_or_UA.setText(f'{UA*100:.2f}')
 
             phi_target = q_target/(Q*UA)
             S_solved = linear_solver(
-                func=phi_parallel_any_br187,
-                dict_params=dict(W_m=W, H_m=H, w_m=0.5*W, h_m=0.5*H, S_m=0),
+                func=phi_perpendicular_any_br187,
+                dict_params=dict(W_m=W, H_m=H, w_m=0., h_m=0., S_m=0),
                 x_name='S_m',
                 y_target=phi_target,
-                x_upper=200,
+                x_upper=1000,
                 x_lower=0.01,
                 y_tol=0.001,
-                iter_max=100,
+                iter_max=500,
                 func_multiplier=-1
             )
-            phi_solved = phi_parallel_any_br187(W_m=W, H_m=H, w_m=0.5*W, h_m=0.5*H, S_m=S_solved)
+            phi_solved = phi_perpendicular_any_br187(W_m=W, H_m=H, w_m=0.5*W, h_m=0.5*H, S_m=S_solved)
             q_solved = Q * phi_solved
 
             self.ui.lineEdit_out_Phi.setText(f'{phi_solved:.4f}')
             self.ui.lineEdit_out_q.setText(f'{q_solved:.2f}')
             self.ui.lineEdit_out_S_or_UA.setText(f'{S_solved/2:.2f}')
 
-            self.repaint()
+        self.repaint()
