3958e1f79c8873eb95f127cda6277b2bff7bfcd2
fuyans <fuyans@gmail.com>
2020-02-05 21:12:56 +0000

    Update gui_build.py

--- pyinstaller_builds/gui_build.py
+++ pyinstaller_builds/gui_build.py
@@ -3,29 +3,26 @@ import os
 import subprocess
 
 
-def build_gui(app_name: str = 'OFR BOX', fp_target_py: str = 'gui.py', options: list = None):
+def build_gui(app_name: str = 'FSEUTIL', fp_target_py: str = 'gui.py', options: list = None):
 
     os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
     cmd_option_list = [
-        f'-n "{app_name}"',
+        f'-n {app_name}',
         "--icon=" + os.path.realpath(os.path.join("etc", "ofr_logo_1_80_80.ico")),
     ]
     if options:
         cmd_option_list.extend(options)
 
-    cmd_options = " ".join(cmd_option_list)
-    cmd_options = cmd_options + " " if len(cmd_options) > 0 else ""
+    cmd = ['pyinstaller'] + cmd_option_list + [fp_target_py]
 
-    cmd = f"pyinstaller {cmd_options}{fp_target_py}"
-
-    subprocess.call(cmd)
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    proc.wait()
 
 
 if __name__ == "__main__":
     build_gui(
         options=[
-            "--noconsole",  # disable console display
             "--onedir",  # output unpacked dist to one directory, including an .exe file
             "--windowed",  # make it a windowed application
             "--noconfirm",  # replace output directory without asking for confirmation
