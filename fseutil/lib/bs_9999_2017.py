146542dd4374ece008103a8d4253986466adeb74
fuyans <fuyans@gmail.com>
2020-02-06 07:57:49 +0000

    Update bs_9999_2017.py

--- fseutil/lib/bs_9999_2017.py
+++ fseutil/lib/bs_9999_2017.py
@@ -9,13 +9,14 @@ def clause_15_6_6_e_merging_flow_1(
 ):
     """Calculates merging flow at final exit level in accordance with Figure 6 (1 of 3) in BD 9999:2017, page 68.
     Merging flow from stair with storey exit at final exit level.
+    SI UNITS unless specified.
 
     :param N: is the number of people served by the final exit level storey exit.
     :param D: is the lesser distance from the final exit level storey exit or the lowest riser from the upward portion of the stair, in metres (m).
-    :param S_up: is the stair width for the upward portion of the stair, in millimetres (mm).
-    :param W_SE: is the width of the final exit level storey exit, in millimetres (mm).
-    :param X: is the minimum door width per person (see 16.6 and Clause 18), in millimetres (mm).
-    :return W_FE: is the width of the final exit, in millimetres (mm).
+    :param S_up: is the stair width for the upward portion of the stair, in millimetres.
+    :param W_SE: is the width of the final exit level storey exit, in millimetres.
+    :param X: is the minimum door width per person (see 16.6 and Clause 18), in millimetres.
+    :return W_FE: is the width of the final exit, in millimetres.
     """
 
     # convert unit
