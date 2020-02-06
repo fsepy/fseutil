e465274d0d59ce36d8e0ea38c3a159df3783b112
fuyans <fuyans@gmail.com>
2020-02-06 17:44:36 +0000

    Update test_fse_flame_height.py

--- fseutil/tests/test_fse_flame_height.py
+++ fseutil/tests/test_fse_flame_height.py
@@ -1,4 +1,4 @@
-from fseutil.lib.fse_flame_height_ import _test_mean_flame_height_pd_7974 as test_mean_flame_height_pd_7974
+from fseutil.lib.fse_flame_height import _test_mean_flame_height_pd_7974 as test_mean_flame_height_pd_7974
 
 
 test_mean_flame_height_pd_7974()
