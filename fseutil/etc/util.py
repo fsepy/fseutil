7f80c9c7accc0f698df312de876df4f92fb94e3f
fuyans <fuyans@gmail.com>
2020-02-12 12:53:46 +0000

    Added a hash function.

---
+++ fseutil/etc/util.py
@@ -0,0 +1,7 @@
+import binascii
+import hashlib
+
+
+def hash_simple(key: bytes, string: str, algorithm: str = 'sha512', length: int = 20):
+    cipher = int(binascii.hexlify(hashlib.pbkdf2_hmac(algorithm, string, key, 100000)), 16) % (10 ** length)
+    return cipher
