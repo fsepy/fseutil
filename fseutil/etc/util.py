eabfe33f96d9c7f7162a8e207e1692644a42f8fe
fuyans <fuyans@gmail.com>
2020-02-12 13:01:26 +0000

    Update util.py

--- fseutil/etc/util.py
+++ fseutil/etc/util.py
@@ -2,6 +2,6 @@ import binascii
 import hashlib
 
 
-def hash_simple(key: bytes, string: str, algorithm: str = 'sha512', length: int = 20):
+def hash_simple(key: bytes, string: bytes, algorithm: str = 'sha512', length: int = 20):
     cipher = int(binascii.hexlify(hashlib.pbkdf2_hmac(algorithm, string, key, 100000)), 16) % (10 ** length)
     return cipher
